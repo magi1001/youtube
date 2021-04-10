4a2d2e1d4bd0289cbd23d288f4bbc580ec5292db
magi1001 <81128297+magi1001@users.noreply.github.com>
2021-04-10 13:19:21 +0900

    Add files via upload

---
+++ Tutorial15_Main.java
@@ -0,0 +1,10 @@
+package justDoIt;
+
+public class Tutorial15_Main {
+	public static void main(String[] args) {
+		Tutorial15_Student student1 = new Tutorial15_Student("홍길동", 20, 189, 86, "13701", 3, 3.0);
+		Tutorial15_Student student2 = new Tutorial15_Student("이순신", 24, 180, 82, "13741", 2, 4.0);
+		student1.show();
+		student2.show();
+	}
+}
